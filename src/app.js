a503629cce4b4549af6d8b66a4eebca60be320bc
Victor Oluborode <oluborodevictor110@gmail.com>
2025-07-13 23:52:13 +0100

    update

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const registerRoute = require("./routes/auth");
+const cors = require('cors');
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3001", // React dev server URL
+    credentials: true,
+  })
+);
 
-app.use("/secureapi/register", registerRoute);
+app.use("/api/auth/register", registerRoute);
 
 
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const registerUser = require('../services/userService')
-const registerValidation = require('../utils/validation')
+const registerValidation = require('../utils/validation');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 router.post("/", registerValidation, async (req, res) => {
+    const { email, password, fullname } = req.body;
     try {
-        const { email, password, fullname } = req.body;
+        const existingUser = await prisma.user.findUnique({
+            where: { email: email }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                error: "Email already in use"
+            })
+        }
         await registerUser(email, password, fullname);
         res.status(200).json({
             success: true,
             message: "user registered"
         })
     } catch (err) {
+        console.log("Registration error:", err)
         res.status(500).json({
             error: "Server error"
         })
