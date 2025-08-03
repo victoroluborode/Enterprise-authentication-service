7da91a8db50a109aa4688d29d4ce0c84da7864b2
Victor Oluborode <oluborodevictor110@gmail.com>
2025-08-03 23:48:58 +0100

    sanitization using Dompurify for xss

--- src/utils/sanitization.js
+++ src/utils/sanitization.js
@@ -1,1 +1,27 @@
-const xss = require("xss");
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
+
+
+const sanitizeFields = (fields) => {
+    return (req, res, next) => {
+        if (!req.body || typeof req.body !== 'object') {
+            return next();
+        }
+    }
+
+    fields.forEach(field => {
+        if (req.body.hasOwnProperty(field) && typeof req.body[field] === 'string') {
+            req.body[field] = DOMPurify.sanitize(req.body[field], {
+                ALLOWED_TAGS: [],
+                ALLOWED_ATTR: []
+            });
+        }
+    });
+
+    next();
+};
+
+module.exports = sanitizeFields;
