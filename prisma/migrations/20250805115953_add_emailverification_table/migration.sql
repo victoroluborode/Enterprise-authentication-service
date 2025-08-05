52c80c47399577f50a0a9705217ee9d2b37e7db0
Victor Oluborode <oluborodevictor110@gmail.com>
2025-08-05 13:25:42 +0100

    created generating email verification token function

---
+++ prisma/migrations/20250805115953_add_emailverification_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "EmailVerificationToken" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "EmailVerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EmailVerificationToken_token_key" ON "EmailVerificationToken"("token");
+
+-- CreateIndex
+CREATE INDEX "EmailVerificationToken_userId_idx" ON "EmailVerificationToken"("userId");
+
+-- AddForeignKey
+ALTER TABLE "EmailVerificationToken" ADD CONSTRAINT "EmailVerificationToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/services/emailTokenService.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient;
+
+const createEmailToken = async (userId) => {
+    const token = crypto.randomBytes(32).toString('hex');
+    const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+    const hashedToken = await bcrypt.hash(token, 10);
+
+    await prisma.emailverificationtoken.create({
+        data: {
+            userId: userId,
+            token: hashedToken,
+            expiresAt: expiresAt
+        },
+    });
+    return token;
+};
+
+
