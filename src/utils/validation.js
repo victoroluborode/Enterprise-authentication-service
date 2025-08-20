114e9b65a54381da668386a6330ab2f1c0614610
Victor Oluborode <oluborodevictor110@gmail.com>
2025-08-20 13:48:10 +0100

    debug

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -8,7 +8,6 @@ const registerValidation = [
             "Password must be at least 8 characters and include uppercase, lowercase, number, and special character"
         ),
   body('fullname').trim().notEmpty().withMessage('Full name is required'),
-  
 
         (req, res, next) => {
             const errors = validationResult(req);
