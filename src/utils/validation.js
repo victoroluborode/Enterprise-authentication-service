0c99b721c32dc7547c98d25538d164bfb2f7fe8a
Victor Oluborode <oluborodevictor110@gmail.com>
2025-08-31 10:02:28 +0100

    deployment setup

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -91,7 +91,7 @@ const forgotPasswordValidation = [
 ];
 
 const resetPasswordValidation = [
-  body("newpassword")
+  body("newPassword")
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/)
     .withMessage(
       "Password must be at least 8 characters and include uppercase, lowercase, number, and special character"
